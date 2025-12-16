6e49672abb81636d8f720f43aae0e2cb2d5ab286
rukigaki <rukigaki@gmail.com>
2025-12-16 17:40:42 +0300

    Create sqlalchemy model user

---
+++ app/db/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    age: Mapped[int]
+    weight: Mapped[float]
+    email: Mapped[str]
