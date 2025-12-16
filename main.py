87c48b67d16299abe460c8057f92a959e691c716
rukigaki <rukigaki@gmail.com>
2025-12-16 15:52:50 +0300

    Base main:app application on FastAPI

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import uvicorn
 from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+async def hello_world():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
 
 
